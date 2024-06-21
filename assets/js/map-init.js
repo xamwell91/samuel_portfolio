2950a23effba6e69d7d93820b328af66dd646c05
samuelthapamagar <samuelthapamagar77@gmail.com>
2024-06-21 16:26:19 +0545

    portfolio complete

--- assets/js/map-init.js
+++ assets/js/map-init.js
@@ -5,7 +5,7 @@
           zoom: 17,
           scrollwheel: false, 
           navigationControl: false,
-          center: new google.maps.LatLng(24.906308,91.870413),
+          center: new google.maps.LatLng(27.710726, 85.324491),
           styles: [{"featureType":"water","elementType":"geometry","stylers":[{"color":"#e9e9e9"},{"lightness":17}]},
           {"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#f5f5f5"},{"lightness":20}]},
           {"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#ffffff"},{"lightness":17}]},
@@ -23,7 +23,7 @@
       var mapElement = document.getElementById('mh-map');
       var map = new google.maps.Map(mapElement, mapOptions);
       var marker = new google.maps.Marker({
-          position: new google.maps.LatLng(24.906308,91.870413),
+          position: new google.maps.LatLng(27.710726, 85.324491),
           map: map,
           title: '24 Golden Tower (2nd floor), Amborkhana, Sylhet.!'
       });
